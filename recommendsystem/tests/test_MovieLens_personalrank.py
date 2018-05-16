a608dcae7beaa16242c8f0a4c402f10d4aba5a3a
CxwAlex <32626258+CxwAlex@users.noreply.github.com>
2018-05-17 00:58:46 +0800

    user_cf, item_cf, personal_rank, random

--- recommendsystem/tests/test_MovieLens_personalrank.py
+++ recommendsystem/tests/test_MovieLens_personalrank.py
@@ -10,13 +10,13 @@ from recommendsystem.TrainAndTestWorkflow import *
 class RecommendAndParameterTest(unittest.TestCase):
     def test_personalrank1k(self):
         read_filepath = project_filepath + '/dataset/MovieLens/1m/ratings_1kuser.dat'
-        data_std = MovieLensRatings2Std(read_filepath,lines=100)
+        data_std = MovieLensRatings2Std(read_filepath)
         recommend_engine = 'RecommendPersonalRank'
         dataset_name = 'MovieLens1M'
         parameters = getparaters(dataset_name, recommend_engine)
         RecommendAndParameterHighSpeed(data_std, recommend_engine, repeat_k=10, parameters=parameters)
 
-'''
+
     def test_personalrank100(self):
         read_filepath = project_filepath + '/dataset/MovieLens/1m/ratings_100user.dat'
         data_std = MovieLensRatings2Std(read_filepath)
@@ -24,4 +24,3 @@ class RecommendAndParameterTest(unittest.TestCase):
         dataset_name = 'MovieLens1M'
         parameters = getparaters(dataset_name, recommend_engine)
         RecommendAndParameterHighSpeed(data_std, recommend_engine, repeat_k=10, parameters=parameters)
-'''
